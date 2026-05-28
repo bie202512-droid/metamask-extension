2ade5b96748a57f7733ba5cfda84f0871c924d63
jeremy-consensys <jeremy.tsang-ext@consensys.net>
2026-05-29 00:18:42 +0700

    style: apply prettier formatting to BTC activity POMs

--- test/e2e/page-objects/pages/home/asset-list.ts
+++ test/e2e/page-objects/pages/home/asset-list.ts
@@ -932,7 +932,9 @@ class AssetListPage extends HomePage {
     networkName: string,
     tab: string = 'Popular',
   ): Promise<void> {
-    console.log(`Selecting only ${networkName} in the asset list network filter`);
+    console.log(
+      `Selecting only ${networkName} in the asset list network filter`,
+    );
     await this.openNetworksFilter();
     const networkManager = new NetworkManager(this.driver);
     await networkManager.selectTab(tab);
